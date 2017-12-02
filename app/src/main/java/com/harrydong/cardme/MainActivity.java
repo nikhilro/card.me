a81d83eab19c6d60758be8113908bec6cc991c7c
yayimahuman <hhdong@uwaterloo.ca>
2017-12-02 14:42:08 -0500

    added recyclerview

--- app/src/main/java/com/harrydong/cardme/MainActivity.java
+++ app/src/main/java/com/harrydong/cardme/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -37,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         View contentView = findViewById(R.id.toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
 
         initializeComponents();
 
