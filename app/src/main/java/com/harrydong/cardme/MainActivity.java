21f6a7e5e56e2d06f11baa4a8afe6a4c72dcd8ab
NivedithaK <nivedithakani@gmail.com>
2017-12-02 14:21:17 -0500

    ...

--- app/src/main/java/com/harrydong/cardme/MainActivity.java
+++ app/src/main/java/com/harrydong/cardme/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar.setOverflowIcon(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_launcher_background));
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This is a snackbar", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
