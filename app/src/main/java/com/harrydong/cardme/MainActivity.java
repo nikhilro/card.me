4f4a0d1b6d9ecc81d8997b5666e57008bbadb846
yayimahuman <hhdong@uwaterloo.ca>
2017-12-02 17:57:45 -0500

    it probably still doesnt work

--- app/src/main/java/com/harrydong/cardme/MainActivity.java
+++ app/src/main/java/com/harrydong/cardme/MainActivity.java
@@ -18,16 +18,37 @@ import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import static android.nfc.NdefRecord.createMime;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentFilter.MalformedMimeTypeException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, NfcAdapter.CreateNdefMessageCallback {
+public class MainActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
     private NfcAdapter mNfcAdapter;
-    private TextView mOutputText;
+    private TextView mTextView;
     private EditText mInputField;
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    public static final String TAG = "NfcDemo";
 
     //this method runs when the app is opened: the "setup" method
     @Override
@@ -38,7 +59,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setSupportActionBar(toolbar);
         View contentView = findViewById(R.id.toolbar);
 
-        initializeComponents();
+        mInputField = (EditText) findViewById(R.id.nfcSend);
+        mTextView = (TextView) findViewById(R.id.nfcMsg);
+
+        //ImageView mTapImageBeam = (ImageView) findViewById(R.id.img_beam);
+        //mTapImageBeam.setOnClickListener(this);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         //provides a notification as a snackbar if no NFC adapter
         if (mNfcAdapter == null) {
@@ -46,58 +73,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Snackbar.make(contentView,"No NFC adapter found.",Snackbar.LENGTH_SHORT);
         }
 
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText("NFC is on!");
+        }
+        handleIntent(getIntent());
+
+
+
         //creates the round button in the corner and assigns a listener to it
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Floating action button clicked!", Snackbar.LENGTH_LONG)
+
+            }
+        });
+
+        Button setBtn = (Button)findViewById(R.id.setBtn);
+        Button receiveBtn = (Button)findViewById(R.id.receiveBtn);
+
+        setBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                sendNfcMsg(view);
+                Snackbar.make(view, "NFC message sent!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-    }
 
-    //this method initializes the elements. This is where the elements in XML will be assigned to java variables.
-    private void initializeComponents() {
-        //mInputField = (EditText) findViewById(R.id.edt_input_beam);
-        //mOutputText = (TextView) findViewById(R.id.received_txt_beam);
-
-        //ImageView mTapImageBeam = (ImageView) findViewById(R.id.img_beam);
-        //mTapImageBeam.setOnClickListener(this);
-
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
 
     @Override
     public void onResume() {
         super.onResume();
-
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            processIntent(getIntent());
-        }
+        setupForegroundDispatch(this, mNfcAdapter);
     }
 
-    void processIntent(Intent intent) {
-        Parcelable[] receivedMessages = intent.getParcelableArrayExtra(
-                NfcAdapter.EXTRA_NDEF_MESSAGES);
-
-        NdefMessage message = (NdefMessage) receivedMessages[0];
-
-        mOutputText.setText(new String(message.getRecords()[0].getPayload()));
-    }
 
     @Override
     public void onPause() {
+        stopForegroundDispatch(this, mNfcAdapter);
+        //mNfcAdapter.disableForegroundDispatch(this);
         super.onPause();
+    }
 
-        mNfcAdapter.disableForegroundDispatch(this);
+    @Override
+    protected void onNewIntent(Intent intent) {
+        //This method gets called, when a new Intent gets associated with the current activity instance.
+        handleIntent(intent);
     }
 
     //this is a function which will be tied to a button to send an NFC message
-    @Override
-    public void onClick(View v) {
+    public void sendNfcMsg(View v) {
         //send message
         //i dont know if this actually works of if the callback needs to be the function below this function
         mNfcAdapter.setNdefPushMessageCallback(this, this);
@@ -113,6 +144,120 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         "application/vnd.com.example.android.beam", text.getBytes())
                 });
     }
+
+    public static void setupForegroundDispatch(final AppCompatActivity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    public static void stopForegroundDispatch(final AppCompatActivity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+    public void handleIntent(Intent intent) {
+        Log.i("i", "handling intent");
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            String type = intent.getType();
+            if (MIME_TEXT_PLAIN.equals(type)) {
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask().execute(tag);
+            } else {
+                Log.d(TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                 new NdefReaderTask().execute(tag);
+                 break;
+                }
+            }
+        }
+    }
+
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+
+        @Override
+        protected String doInBackground(Tag... params) {
+            Tag tag = params[0];
+
+            Ndef ndef = Ndef.get(tag);
+            if (ndef == null) {
+                // NDEF is not supported by this Tag.
+                return null;
+            }
+
+            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+            NdefRecord[] records = ndefMessage.getRecords();
+            for (NdefRecord ndefRecord : records) {
+                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                    try {
+                        return readText(ndefRecord);
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e(TAG, "Unsupported Encoding", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+            byte[] payload = record.getPayload();
+
+            // Get the Text Encoding
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+            // Get the Language Code
+            int languageCodeLength = payload[0] & 0063;
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                mTextView.setText("Read content: " + result);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -136,8 +281,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-//    /** Called when the user taps the Send button */
-//    public void sendMessage(View view) {
-//        // Do something in response to button
-//    }
 }
