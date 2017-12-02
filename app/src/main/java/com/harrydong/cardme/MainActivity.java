6991b137abdd02a66816548293c5bd83aaab9226
Nikhil Gupta <niks.gupta09@gmail.com>
2017-12-02 18:43:49 -0500

    prepping for demo

--- app/src/main/java/com/harrydong/cardme/MainActivity.java
+++ app/src/main/java/com/harrydong/cardme/MainActivity.java
@@ -1,6 +1,5 @@
 package com.harrydong.cardme;
 
-import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,29 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.content.ContextCompat;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
-import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import static android.nfc.NdefRecord.createMime;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-import com.google.gson.Gson;
-
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, NfcAdapter.CreateNdefMessageCallback {
@@ -63,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, DemoActivity.class);
+                Intent intent = new Intent(MainActivity.this, DemoNik.class);
                 startActivity(intent);
 //                Snackbar.make(view, "Floating action button clicked!", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
